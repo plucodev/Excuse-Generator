667fda23dcb373956b7af490b08b95499f450ba4
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-05-06 18:49:45 +0000

    refactoring code

--- script.js
+++ script.js
@@ -2,23 +2,18 @@
     let verb = ['eat','pissed','crushed','broked'];
     let what = ['my homework', 'my pants', 'my shoes', 'my car']
     let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
-    
-    function excuseGenerator(){
-        let excuseGeneratorArray = []
-        let randomItemWho = who[Math.floor(Math.random()*who.length)];
-            excuseGeneratorArray.push(randomItemWho)
-        let randomItemVerb = verb[Math.floor(Math.random()*verb.length)];
-            excuseGeneratorArray.push(randomItemVerb)
-        let randomItemWhat = what[Math.floor(Math.random()*what.length)];
-            excuseGeneratorArray.push(randomItemWhat)
-        let randomItemWhen = when[Math.floor(Math.random()*when.length)];
-            excuseGeneratorArray.push(randomItemWhen)
-        let excuseText = excuseGeneratorArray[0] + " " + excuseGeneratorArray[1] + " " + excuseGeneratorArray[2] + " " + excuseGeneratorArray[3]
-        
-        document.getElementById("excuse-text").innerHTML = excuseText;
-        
-        return excuseText
 
+    function randomN(arr){
+        return arr[Math.floor(Math.random()*arr.length)];
     }
-    
-    
+    function excuseGenerator(){
+        let randomItemWho = randomN(who);
+        let randomItemVerb = randomN(verb);
+        let randomItemWhat = randomN(what);
+        let randomItemWhen = randomN(when);
+        document.getElementById("excuse-text").innerHTML = randomItemWho + " " + randomItemVerb + " " + randomItemWhat + " " + randomItemWhen;
+
+        return excuseText
+    }
+
+
